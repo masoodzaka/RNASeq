79a103a3692316446f509d6f2a24a6a66d514bc9
masood zaka <masood6985@gmail.com>
2022-06-19 17:42:23 +0000

    Extended functions of salmon and star aligner inputs for trimmed fastqs

--- SCRIPTS/functions.py
+++ SCRIPTS/functions.py
@@ -12,19 +12,19 @@ from snakemake.utils import min_version
 
 configfile: "config.yaml"
 
-MASTER_LIST = pd.read_table(config["MASTER_LIST"], dtype=str).set_index(["sample","lane"], drop=False)
+MASTER_LIST = pd.read_table(config["MASTER_LIST"], dtype=str).set_index(["sample","runID"], drop=False)
 MASTER_LIST.index = MASTER_LIST.index.set_levels([i.astype(str) for i in MASTER_LIST.index.levels])
 
 # samples list from master_list
 
 SAMPLES = [ sample for sample in MASTER_LIST["sample"]]
-LANES = [ runid for runid in MASTER_LIST["lane"]]
+RUNIDS = [ runID for runID in MASTER_LIST["runID"]]
 
 # Global wildcard constraints
 
 wildcard_constraints:
 	sample = "|".join(MASTER_LIST["sample"]),
-	lane = "|".join(MASTER_LIST["lane"]),
+	runID = "|".join(MASTER_LIST["runID"]),
 
 
 
@@ -33,14 +33,14 @@ wildcard_constraints:
 def fastqc_input(wildcards):
 	""" if trimming use following dictionary"""
 	if config["CUTADAPT"]["trimming"]:
-		return {"R1": "FASTQ/TRIMMED/{sample}_{lane}_R1.fastq.gz".format(**wildcards),
-		"R2":"FASTQ/TRIMMED/{sample}_{lane}_R2.fastq.gz".format(**wildcards)}
+		return {"R1": "FASTQ/TRIMMED/{sample}_{runID}_R1.fastq.gz".format(**wildcards),
+		"R2":"FASTQ/TRIMMED/{sample}_{runID}_R2.fastq.gz".format(**wildcards)}
 	else:
 		"""
 		function provides the rowwise fastqs from the master list in the form a python dictionary
 		if no trimming use raw fastq files
 		"""
-		fastqs = MASTER_LIST.loc[(wildcards.sample, wildcards.lane), ["fastq1", "fastq2"]].dropna()
+		fastqs = MASTER_LIST.loc[(wildcards.sample, wildcards.runID), ["fastq1", "fastq2"]].dropna()
 		if len(fastqs) == 2:
 			return {"R1": fastqs.fastq1, "R2": fastqs.fastq2}
 		else:
@@ -48,7 +48,7 @@ def fastqc_input(wildcards):
 
 def cutadapt_input(wildcards):
 		""" function provides the rowwise fastqs from the master list in the form a python dictionary"""
-		fastqs = MASTER_LIST.loc[(wildcards.sample, wildcards.lane), ["fastq1", "fastq2"]].dropna()
+		fastqs = MASTER_LIST.loc[(wildcards.sample, wildcards.runID), ["fastq1", "fastq2"]].dropna()
 		if len(fastqs) == 2:
 			return {"R1": fastqs.fastq1, "R2": fastqs.fastq2}
 		else:
@@ -64,13 +64,32 @@ def base_name(filepath):
 	return base
 
 def multiqc_input(wildcards):
-	return expand(["QC/TRIMMED/{sample}_{lane}.paired.qc.txt","QC/FASTQC/{sample}_{lane}_R1_fastqc.zip"], zip,sample=SAMPLES, lane=LANES)
+	if config["CUTADAPT"]["trimming"]:
+		return expand(["QC/TRIMMED/{sample}_{runID}.paired.qc.txt",
+			"QC/FASTQC/{sample}_{runID}_R1_fastqc.zip",
+			"QC/FASTQC/{sample}_{runID}_R2_fastqc.zip"],zip, sample=SAMPLES, runID=RUNIDS)
+	else:
+		return expand(["QC/FASTQC/{sample}_{runID}_R1_fastqc.zip",
+			"QC/FASTQC/{sample}_{runID}_R2_fastqc.zip"],zip, sample=SAMPLES, runID=RUNIDS)
 
 def salmon_input(wildcards):
-	fastqs = MASTER_LIST.loc[(wildcards.sample), ["fastq1", "fastq2"]].dropna()
-	return {"R1": [f for f in fastqs.fastq1], "R2": [f for f in fastqs.fastq2]}
+	if not config["CUTADAPT"]["trimming"]:
+		fastqs = MASTER_LIST.loc[(wildcards.sample), ["fastq1", "fastq2"]].dropna()
+		return {"R1": [f for f in fastqs.fastq1], "R2": [f for f in fastqs.fastq2]}
+	else:
+		fastqs = MASTER_LIST.loc[(wildcards.sample), ["fastq1", "fastq2"]].dropna()
+		runs = fastqs.index.tolist()
+		return {"R1":expand("FASTQ/TRIMMED/{sample}_{runID}_R1.fastq.gz",runID=runs, **wildcards),
+		"R2":expand("FASTQ/TRIMMED/{sample}_{runID}_R2.fastq.gz",runID=runs, **wildcards)}
+
 
 
 def star_input(wildcards):
-	fastqs = MASTER_LIST.loc[(wildcards.sample), ["fastq1", "fastq2"]].dropna()
-	return {"R1": [f for f in fastqs.fastq1], "R2": [f for f in fastqs.fastq2]}
+	if not config["CUTADAPT"]["trimming"]:
+		fastqs = MASTER_LIST.loc[(wildcards.sample), ["fastq1", "fastq2"]].dropna()
+		return {"R1": [f for f in fastqs.fastq1], "R2": [f for f in fastqs.fastq2]}
+	else:
+		fastqs = MASTER_LIST.loc[(wildcards.sample), ["fastq1", "fastq2"]].dropna()
+		runs = fastqs.index.tolist()
+		return {"R1":expand("FASTQ/TRIMMED/{sample}_{runID}_R1.fastq.gz",runID=runs, **wildcards),
+		"R2":expand("FASTQ/TRIMMED/{sample}_{runID}_R2.fastq.gz",runID=runs, **wildcards)}
